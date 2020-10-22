49282d11e970e12bddda1f9f7b9c517f3d5e976c
SeoYuzuki <st89200@gmail.com>
2020-10-22 16:04:01 +0800

    重構

--- _h1/src/main/controller/_main.java
+++ _h1/src/main/controller/_main.java
@@ -10,8 +10,7 @@ import main.frameWork.MyHTTPServerBuilder;
 import net.sf.cglib.proxy.Enhancer;
 
 public class _main {
-    public static void main(String[] args) throws IOException {       
-        
+    public static void main(String[] args) throws IOException {
         String version = System.getProperty("java.version");
         System.out.println("java.version:" + version);
         MyHTTPServer myHTTPServer2 = MyHTTPServerBuilder.newMyHTTPServer()
